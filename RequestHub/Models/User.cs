2eef7263cdd15656a9156fb0aff245a2fe56b47f
crystall 6ix <4a4auap@mail.ru>
2026-05-14 05:46:05 +0500

    start

---
+++ RequestHub/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace RequestHub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string HashPassword { get; set; } = string.Empty;
+        public string Role { get; set; } = "Requested";
+    }
+}
